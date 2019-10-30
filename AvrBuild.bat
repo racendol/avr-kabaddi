daf4cf785c74fea5c3f72e07a7e5d2ca6057d9ab
racendol <dragon.arkan259@gmail.com>
2019-10-31 01:48:23 +0700

    Added gitignore

--- AvrBuild.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-"D:\avrstudio\AvrAssembler2\avrasm2.exe" -S "C:\Users\Rocky\Documents\pok sandbox\avr-kabaddi\labels.tmp" -fI -W+ie -o "C:\Users\Rocky\Documents\pok sandbox\avr-kabaddi\avr-kabaddi.hex" -d "C:\Users\Rocky\Documents\pok sandbox\avr-kabaddi\avr-kabaddi.obj" -e "C:\Users\Rocky\Documents\pok sandbox\avr-kabaddi\avr-kabaddi.eep" -m "C:\Users\Rocky\Documents\pok sandbox\avr-kabaddi\avr-kabaddi.map" "C:\Users\Rocky\Documents\pok sandbox\avr-kabaddi\avr-kabaddi.asm"
